2d5ca6b351f028eebe879912f9e641c2fed0ca51
Neil Moffatt <neil.moffatt@outlook.com>
2025-03-16 20:16:26 -0700

    basic server completed

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+
+	"net/http"
+)
+
 func main() {
+	fmt.Println("Welcome!")
+	server := gin.Default()
+	server.GET("/events", getEvents)
+	server.Run(":8080") // localhost
+
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "All good!"})
 
 }
